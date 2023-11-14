645fc9683b52676eab8da486ddcc41138169c289
JulioCesar-A <145122101+JulioCesar-A@users.noreply.github.com>
2023-11-14 17:27:11 -0300

    Update main.py
    CRUD
    
    Create - pronto

--- main.py
+++ main.py
@@ -6,14 +6,24 @@ banco = mysql.connector.connect(
     password= '302302',
     database= 'academia'
 )
-
 meucursor = banco.cursor()
-pesquisa = 'select * from alunos;'
-meucursor.execute(pesquisa)
-resultado = meucursor.fetchall()
 
-for x in resultado:
-    print(x)
+inicio = input("Deseja iniciar alguma ação? (s/n) -> ")
+while inicio == "s" or inicio == "S":
+    menu = input("Qual ação deseja realizar?\n(1- Inserir | 2- Ler | 3- Atualizar | 4- Deletar) -> ")
+    while menu == "1" or menu == "2" or menu == "3" or menu == "4":
+        #Create
+        if menu == "1":
+            nome_aluno = input("Informe o nome do aluno: ")
+            cpf_aluno = input("Informe o CPF do aluno: ")
+            telefone_aluno = input("Informe o telefone do aluno: ")
+            endereço_aluno = input("Informe o endereço do aluno: ")
+            comando = f'INSERT INTO alunos VALUES ("{nome_aluno}","{cpf_aluno}","{telefone_aluno}","{endereço_aluno}")'
+            meucursor.execute(comando)
+            banco.commit()
+            menu = input("Qual ação deseja realizar?\n(1- Inserir | 2- Ler | 3- Atualizar | 4- Deletar | 5- Encerrar) -> ")
+        #Read
+        if menu == "2":
 
 meucursor.close()
 banco.close()

21350d83252ab231e9f65eddc0173f0c447898cf
JulioCesar-A <145122101+JulioCesar-A@users.noreply.github.com>
2023-11-16 20:22:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 import mysql.connector
 
 banco = mysql.connector.connect(
-    host= 'localhost',
-    user= 'root',
-    password= '302302',
-    database= 'academia'
+    host = "localhost",
+    user = "root",
+    password = "302302"
+    database = "livraria"
 )
-meucursor = banco.cursor()
-
 while True:
     menu = input("Qual ação deseja realizar?\n(1- Inserir | 2- Ler | 3- Atualizar | 4- Deletar)\nAção escolhida: ")
     if menu == "1":
@@ -32,15 +30,126 @@ while True:
             Nome_Editora = input("Informe o nome da editora: ")
             Fundacao_Editora = input("Informe o ano de fundação da editora: ")
             Num_Selos = input("Informe o número de selos da editora: ")
-            
-            
-            
+        else:
+            print("Opção inválida")        
     elif menu == "2":
+        tabela = input("\nQual tabela você deseja visualizar? ")
+        if tabela == "livros" or tabela == "livros".upper() or tabela == "livros".capitalize():
+            leitura = f'SELECT * FROM {tabela}'
+            meucursor.execute(leitura)
+            resultado = meucursor.fetchall()
+            for x in resultado:
+                print(x)
+        elif tabela == "autores" or tabela == "autores".upper() or tabela == "autores".capitalize():
+            leitura = f'SELECT * FROM {tabela}'
+            meucursor.execute(leitura)
+            resultado = meucursor.fetchall()
+            for x in resultado:
+                print(x)
+        elif tabela == "editora" or tabela == "editora".upper() or tabela == "editora".capitalize():
+            leitura = f'SELECT * FROM {tabela}'
+            meucursor.execute(leitura)
+            resultado = meucursor.fetchall()
+            for x in resultado:
+                print(x)
+        elif tabela == "publicados" or tabela == "publicados".upper() or tabela == "publicados".capitalize():
+            leitura = f'SELECT * FROM {tabela}'
+            meucursor.execute(leitura)
+            resultado = meucursor.fetchall()
+            for x in resultado:
+                print(x)
+        else:
+            print("Opção inválida")
+    elif menu == "3":
+        tabela = input("\nQual tabela você deseja alterar? ")    
+        if tabela == "livros" or tabela == "livros".upper() or tabela == "livros".capitalize():
+            coluna = input("O que você deseja alterar? ")
+            if coluna in "nomeNOME"
+                antigo_nome = input("Informe o antigo nome: ")
+                novo_nome = input("Informe o novo nome: ")
+                atualizar = f'UPDATE {tabela} SET Nome_Livro = "{novo_nome}" WHERE Nome_Livro = "{antigo_nome}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            elif coluna in "generoGENERO":
+                antigo_genero = input("Informe o antigo genero: ")
+                novo_genero = input("Informe o novo genero: ")
+                atualizar = f'UPDATE {tabela} SET Gênero = "{novo_genero}" WHERE Gênero = "{antigo_genero}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            elif coluna in "anoANO":
+                antigo_ano = input("Informe o antigo ano: ")
+                novo_ano = input("Informe o novo ano: ")
+                atualizar = f'UPDATE {tabela} SET Ano_Publicação = "{novo_ano}" WHERE Ano_Publicação = "{antigo_ano}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            elif coluna in "valorVALOR":
+                antigo_valor = input("Informe o antigo valor: ")
+                novo_valor = input("Informe o novo valor: ")
+                atualizar = f'UPDATE {tabela} SET Valor_Livro = "{novo_valor}" WHERE Valor_Livro = "{antigo_valor}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            else:
+                print("Opção inválida")
+        elif tabela == "autores" or tabela == "autores".upper() or tabela == "autores".capitalize():
+            coluna = input("O que você deseja alterar? ")
+            if coluna in "nomeNOME"
+                antigo_nome = input("Informe o antigo nome: ")
+                novo_nome = input("Informe o novo nome: ")
+                atualizar = f'UPDATE {tabela} SET Nome_Autor = "{novo_nome}" WHERE Nome_Autor = "{antigo_nome}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            elif coluna in "nacionalidadeNACIONALIDADE":
+                antigo_nacional = input("Informe a antiga nacionalidade: ")
+                novo_nacional = input("Informe a nova nacionalidade: ")
+                atualizar = f'UPDATE {tabela} SET Nacionalidade_Autor = "{novo_nacional}" WHERE Nacionalidade_Autor = "{antigo_nacional}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            elif coluna in "anoANO":
+                opcao = input("Informe qual ano será alterado\n(N- Nascimento |F- Falecimento)")
+                if opcao == "f" or opcao == "F":
+                    antigo_ano = input("Informe o antigo ano de falecimento: ")
+                    novo_ano = input("Informe o novo ano de falecimento: ")
+                    atualizar = f'UPDATE {tabela} SET Falecimento_Autor = "{novo_ano}" WHERE Falecimento_Autor = "{antigo_ano}"'
+                    meucursor.execute(atualizar)
+                    banco.commit()
+                elif opcao == "n" or opcao == "n":
+                    antigo_ano = input("Informe o antigo ano de nascimento: ")
+                    novo_ano = input("Informe o novo ano de Nascimento: ")
+                    atualizar = f'UPDATE {tabela} SET Nascimento_Autor = "{novo_ano}" WHERE Nascimento_Autor = "{antigo_ano}"'
+                    meucursor.execute(atualizar)
+                    banco.commit()
+                else:
+                    print("Opção inválida")
+            elif coluna in "valorVALOR":
+                antigo_valor = input("Informe o antigo valor: ")
+                novo_valor = input("Informe o novo valor: ")
+                atualizar = f'UPDATE {tabela} SET Valor_Livro = "{novo_valor}" WHERE Valor_Livro = "{antigo_valor}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            else:
+                print("Opção inválida")
+        elif tabela == "editora" or tabela == "editora".upper() or tabela == "editora".capitalize():
+            coluna = input("O que você deseja alterar? ")
+            elif coluna in "nomeNOME":
+                antigo_nome = input("Informe o antigo nome: ")
+                novo_nome = input("Informe o novo nome: ")
+                atualizar = f'UPDATE {tabela} SET Nome_Editora = "{novo_nome}" WHERE Nome_Editora = "{antigo_nome}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            elif coluna in "anoANO":
+                antigo_ano = input("Informe o antigo ano: ")
+                novo_ano = input("Informe o novo ano: ")
+                atualizar = f'UPDATE {tabela} SET Fundação_Editora = "{novo_ano}" WHERE Fundação_Editora = "{antigo_ano}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+            elif coluna in "selosSELOS":
+                antigo_selo = input("Informe o antigo número de selos: ")
+                novo_selo = input("Informe o novo número de selos: ")
+                atualizar = f'UPDATE {tabela} SET Num_Selos = "{novo_selo}" WHERE Num_Selos = "{antigo_selo}"'
+                meucursor.execute(atualizar)
+                banco.commit()
+        #UPDATE da tabela publicados        
+        #elif tabela == "publicados" or tabela == "publicados".upper() or tabela == "publicados".capitalize():
 
-
-    elif menu == "2":
-
-
-    elif menu == "2":
-meucursor.close()
-banco.close()
+        
+        
